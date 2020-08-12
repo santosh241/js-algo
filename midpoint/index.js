4cda1bdab84f956c067dfea2c1111de699099e7c
santosh241 <54933982+santosh241@users.noreply.github.com>
2020-08-12 12:40:16 +0530

    Add files via upload

---
+++ midpoint/index.js
@@ -0,0 +1,15 @@
+
+
+function midpoint(list) {
+  let slow = list.getFirst();
+  let fast = list.getFirst();
+
+  while (fast.next && fast.next.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+
+  return slow;
+}
+
+module.exports = midpoint;
---
+++ reverseint.js
@@ -0,0 +1,13 @@
+
+
+function reverseInt(n) {
+  const reversed = n
+    .toString()
+    .split('')
+    .reverse()
+    .join('');
+
+  return parseInt(reversed) * Math.sign(n);
+}
+
+module.exports = reverseInt;
